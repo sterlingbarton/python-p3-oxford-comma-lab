e10ad414befc251110a0295eea0d233fd6c40d27
Sterling <sterlingbarton81@gmail.com>
2023-05-05 09:14:05 -0600

    completed lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,7 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 2:
+        return " and ".join(items)
+    elif len(items) >= 3:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
+    else:
+        return ", ".join(items)
